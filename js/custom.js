b02c8c6387f50ab8e8ca522b960ab7001f1cbe1b
Vladimir Ordenko <ordenko.v@gmail.com>
2019-04-23 16:52:24 +0300

    Last commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+  $('nav').on('click', function() {
+    if ($(this).hasClass('down')) {
+      var movePos = $(window).scrollTop() + $(window).height();
+    }
+    $('html, body').animate({
+      scrollTop: movePos}, 600);
+  });
+});
